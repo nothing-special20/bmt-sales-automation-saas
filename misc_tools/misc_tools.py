2c0653b768b8b64f738976a4ba284466ea547493
Rob <>
2022-04-15 21:05:25 -0400

    fixed messed up paths - way to go retard

--- misc_tools/misc_tools.py
+++ misc_tools/misc_tools.py
@@ -3,6 +3,14 @@ import re
 import shutil
 import os
 
+def extract_static_path(x):
+    if ('% static' in x):
+        output = re.findall("'.{1,255}?'", x)[0]
+        output = re.sub("'", "", output)
+        return output
+    else:
+        return x
+
 # copy all of the file dependencies for a file from one folder to another
 def copy_dependencies(file, src_folder, dest_folder):
     data = open(file, 'r').read()
@@ -11,7 +19,8 @@ def copy_dependencies(file, src_folder, dest_folder):
     keep_file_exts = ['.css', '.js', '.svg', '.png']
     keep_file_exts = ['\\' + x for x in keep_file_exts]
     keep_file_exts = '|'.join(keep_file_exts)
-
+    # print(extract_static_path("% static 'js/sb-admin-2.min.js' %}"))
+    
     file_exts = []
     for x in file_paths:
         try:
@@ -23,6 +32,8 @@ def copy_dependencies(file, src_folder, dest_folder):
 
     file_paths = [x for x in file_paths if bool(re.findall(keep_file_exts, x))]
     file_paths = [x for x in file_paths if x[0]!='.']
+    file_paths = [extract_static_path(x) for x in file_paths]
+    file_paths = [x for x in file_paths if 'custom' not in x]
 
     file_paths = [re.sub('vendor/bulkit', '', x) for x in file_paths]
 
@@ -35,7 +46,10 @@ def copy_dependencies(file, src_folder, dest_folder):
         dst_subfolder = os.path.dirname(dst)
         Path(dst_subfolder).mkdir(parents=True, exist_ok=True)
         shutil.copyfile(src, dst)
-        print(x)
+        # try:
+        #     shutil.copyfile(src, dst)
+        # except:
+        #     print('Err! File not found!\t' + src)
 
 # swaps out things like href="lol.png" for href="{ % static 'lol.png' %}"
 def django_static_file_ref_update(file):
@@ -78,8 +92,9 @@ def django_static_file_ref_update(file):
 
 
 if __name__ == '__main__':
-    file = '/Users/rapple2018/Documents/Professional/Entrepreneur/Bill More Tech/bmt-sales-automation-saas/templates/web/components/bulkit_body_old.html'
+    # file = '/Users/rapple2018/Documents/Professional/Entrepreneur/Bill More Tech/bmt-sales-automation-saas/templates/web/components/bulkit_body_old.html'
+    file = '/Users/rapple2018/Documents/Professional/Entrepreneur/Bill More Tech/bmt-sales-automation-saas/templates/vendor/bootstrap/surveys_body.html'
     src_folder = '/Users/rapple2018/Documents/Professional/Entrepreneur/Bill More Tech/tools/themeforest-Y3pCVj8L-bulkit-agency-startup-and-saas-template/precompiled/assets/'
-    dest_folder = '/Users/rapple2018/Documents/Professional/Entrepreneur/Bill More Tech/bmt-sales-automation-saas/static/vendor/bulkit'
-    # copy_dependencies(file, src_folder, dest_folder)
-    django_static_file_ref_update(file)
+    dest_folder = '/Users/rapple2018/Documents/Professional/Entrepreneur/Bill More Tech/bmt-sales-automation-saas/static/vendor/bulkit/'
+    copy_dependencies(file, src_folder, dest_folder)
+    # django_static_file_ref_update(file)
