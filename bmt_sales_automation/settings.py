a7176c978e23bf71e360d2ed05968c1129791925
Rob <>
2022-04-21 07:46:03 -0400

    fucking static files

--- bmt_sales_automation/settings.py
+++ bmt_sales_automation/settings.py
@@ -52,7 +52,6 @@ THIRD_PARTY_APPS = [
     'rest_framework_api_key',
     'celery_progress',
     'djstripe',  # stripe integration
-    'whitenoise.runserver_nostatic',
 ]
 
 PEGASUS_APPS = [
@@ -208,7 +207,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static_root'
+# STATIC_ROOT = BASE_DIR / 'static_root'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
